bc11ce80a3b2b746f8d4c1419b650999ab62be93
Christian Hug <73828990+hugchristian@users.noreply.github.com>
2021-03-01 14:59:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing App Store apps..."
-mas install 1475387142 # Tailscale
 mas install 441258766 # Magnet
 mas install 425424353 # The Unarchiver
 mas install 872698314 # MoneyMoney
@@ -28,23 +27,26 @@ mas install 1079833326 # Receipts
 mas install 823766827 # OneDrive
 mas install 904280696 # Things
 mas install 1147396723 # WhatsApp
+mas install 409201541 # Pages
 mas install 1176074088 # Termius
 mas install 901110441 # Ninox Database
+mas install 1475387142 # Tailscale
 
 
 echo "Installing Homebrew apps..."
 brew install ansible
 brew install helm
 #brew install kubectl
-brew install hashicorp/tap/terraform
 brew install magic-wormhole
 brew install terminal-notifier
+brew install hashicorp/tap/terraform
 brew install youtube-dl
 
 echo "Installing brew cask apps..."
 brew install arq
 brew install autodesk-fusion360
 brew install bartender
+brew install brooklyn
 brew install discord
 #brew install docker --cask
 brew install figma
@@ -62,7 +64,6 @@ brew install openconnect-gui
 brew install openvpn-connect
 brew install parsec
 brew install resilio-sync
-brew install tor-browser
 brew install veracrypt
 brew install visual-studio-code
 brew install vlc

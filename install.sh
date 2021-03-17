a6cf05befff3c2682f99677f1e6be2e142216be8
Christian Hug <73828990+hugchristian@users.noreply.github.com>
2021-03-17 12:36:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,21 +23,19 @@ mas install 441258766 # Magnet
 mas install 425424353 # The Unarchiver
 mas install 872698314 # MoneyMoney
 mas install 1295203466 # Microsoft Remote Desktop
+mas install 1469182443 # SnipperApp 2
 mas install 1079833326 # Receipts
-mas install 823766827 # OneDrive
 mas install 904280696 # Things
 mas install 1147396723 # WhatsApp
 mas install 409201541 # Pages
 mas install 1176074088 # Termius
 mas install 901110441 # Ninox Database
-mas install 1475387142 # Tailscale
 
 
 echo "Installing Homebrew apps..."
-brew install ansible
-brew install helm
 #brew install kubernetes-cli
 brew install magic-wormhole
+brew install minikube
 brew install terminal-notifier
 brew install terraform
 brew install youtube-dl
@@ -55,14 +53,17 @@ brew install github
 brew install gobdokumente
 brew install google-chrome
 brew install gpg-suite
+brew install intellij-idea-ce
+brew install keepingyouawake
 brew install launchpad-manager
 #brew install mactex
-#brew install minikube
 brew install obs
-brew install openconnect-gui
 brew install openvpn-connect
 brew install parsec
 brew install resilio-sync
+brew install silicon --cask
+brew install tor-browser
+brew install utm
 brew install veracrypt
 brew install visual-studio-code
 brew install vlc
